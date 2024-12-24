dbe2e35ac402b6bd76afb1adde8dc5a692a01bae
Vyacheslav <62720345+az6ru@users.noreply.github.com>
2024-12-24 11:43:33 +0000

    add replit.nix

--- replit.nix
+++ replit.nix
@@ -1,6 +1,12 @@
-{pkgs}: {
+{ pkgs }: {
   deps = [
+    pkgs.python3
+    pkgs.python3Packages.pip
     pkgs.postgresql
     pkgs.openssl
   ];
+
+  shellHook = ''
+    pip install flask requests psycopg2-binary
+  '';
 }
